670b38add1ee6d67e4940e4334b7b9f71cbe5b09
Leah Le <phnganh.le@gmail.com>
2023-03-22 09:26:34 -0400

    Initial Commit

---
+++ bereal-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bereal-clone
+//
+//  Created by Phuong Anh Le on 3/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
